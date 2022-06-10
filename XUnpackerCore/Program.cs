de905134a07f91de5561c2d830c4a4a86e517122
Shivum <shivumkumar1@gmail.com>
2022-06-10 06:53:57 +0000

    Added Basic Usage

--- XUnpackerCore/Program.cs
+++ XUnpackerCore/Program.cs
@@ -4,9 +4,23 @@
     {
         static void Main(string[] args)
         {
-            //Temporary hardcode
-            //XFileHandler.Pack("Test/", "OutTest/base.x");
-            XFileHandler.UnPack("OutTest/base.x", "Test2/");
+            if(args.Length == 0)
+            {
+                Console.WriteLine("Please provide a file to unpack.");
+                return;
+            }
+            if(args[0].Equals("P"))
+            {
+                XFileHandler.Pack(args[1], args[2]);
+            }
+            else if(args[0].Equals("U"))
+            {
+                XFileHandler.UnPack(args[1], args[2]);
+            }
+            else
+            {
+                Console.WriteLine("Please provide a valid command.");
+            }
         }
     }
 }
